82223655d8deddfe7894b6ecc161c5e8cfac700e
Armin Hirschfeld <armin.hirschfeld@gmail.com>
2024-01-15 15:53:07 +0200

    added form in frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,5 @@
 import RequestButton from "@/components/custom/RequestButton";
+import SimpleTestForm from "@/components/form/SimpleTestForm";
 import Container from "@/components/general/Container";
 import Section from "@/components/general/Section";
 
@@ -8,7 +9,15 @@ export default function Home() {
     <main>
       <Section>
         <Container>
+          <h2 className="mb-2">Simple test request</h2>
+          <p className="mb-4 text-gray-400">Timestamp now | View Content | USD 142.52</p>
           <RequestButton />
+        </Container>
+
+        <Container>
+          <h2 className="mb-2">Form test request</h2>
+          <p className="mb-4 text-gray-400">Timestamp now | Username, email, phone</p>
+          <SimpleTestForm />
         </Container>
       </Section>
     </main>
