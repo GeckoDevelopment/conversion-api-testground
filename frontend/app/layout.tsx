952961a9a5b5e0fc3e26a5670bfcf023134f1281
Armin Hirschfeld <armin.hirschfeld@gmail.com>
2024-03-15 18:25:29 +0200

    GTM setup

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.scss'
 import FacebookPixel from '../components/tracking/FacebookPixel'
+import GoogleTagManager from '@/components/custom/GoogleTagManager';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,9 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <GoogleTagManager containterId='GTM-WX85R83R' />
+      </head>
       <body className={inter.className}>
         {children}
-        <FacebookPixel />
       </body>
     </html>
   )
