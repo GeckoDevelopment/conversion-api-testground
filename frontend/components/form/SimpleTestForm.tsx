a7a9578f4f3f81b969ad484a78f9963fce545f8b
Armin Hirschfeld <armin.hirschfeld@gmail.com>
2024-03-17 11:44:39 +0200

    added window layer function

--- frontend/components/form/SimpleTestForm.tsx
+++ frontend/components/form/SimpleTestForm.tsx
@@ -18,6 +18,16 @@ export const formSchema = z.object({
   phone: z.string()
 })
 
+type WindowWithDataLayer = Window & {
+  dataLayer: Record<string, any>[];
+};
+declare const window: WindowWithDataLayer;
+export const pushToDataLayer = (successEventName: string) => {
+  window.dataLayer.push({
+    event: successEventName,
+  });
+};
+
 interface Props {}
 
 const SimpleTestForm: FC<Props> = () => {
